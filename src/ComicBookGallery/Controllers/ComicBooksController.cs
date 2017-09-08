d98e47b12c73bafc0de5c13479f81e88e76e7a4e
Zakawat <subzwary@gmail.com>
2017-09-08 09:42:48 +0500

    Updated ComicBookController to use repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,24 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
 
-            var comicBook = new ComicBook()
-                {
-                    SeriesTitle = "The Amazing Spider-Man",
-                    IssueNumber = 700,
-                    DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                    Artists = new Artist[]
-                    {
-                        new Artist() { Role = "Script" ,    Name = "Dan Slott" },
-                        new Artist() { Role = "Pencils" ,   Name = "Humberto Ramos" },
-                        new Artist() { Role = "Inks" ,      Name = "Victor Olazaba" },
-                        new Artist() { Role = "Colors" ,    Name = "Edgar Delgado" },
-                        new Artist() { Role = "Letters" ,   Name = "Chris Eliopoulos" },
-                    }
-                };
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id); //id.Value OR (int)id can be used
             
             return View(comicBook);
         }
